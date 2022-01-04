492f4daec75731da4fe6ea64f8dcc97009cb0456
tarieltarik <tari.ar@mail.ru>
2022-01-04 22:26:04 +0600

    second commit

--- src/main/java/user/crud/controller/UserController.java
+++ src/main/java/user/crud/controller/UserController.java
@@ -77,11 +77,6 @@ public class UserController {
 
     @GetMapping("/hello")
     public String printWelcome(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("I'm Spring MVC-SECURITY application");
-        messages.add("5.2.0 version by sep'19 ");
-        model.addAttribute("messages", messages);
         return "hello";
     }
 
